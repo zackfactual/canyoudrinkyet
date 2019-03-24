a3919e6703107487b5100557ea39a21c71a99dab
Zachary Velcoff <zackfactual@gmail.com>
2019-03-24 09:25:30 -0600

    initial commit

---
+++ js/canyoudrinkyet.js
@@ -0,0 +1,9 @@
+/*
+-input user age
+-input or detect user time zone
+-compare age to 21 years
+    -if > display completed countdown clock & message 
+    -if < display countdown clock & message (if a birthday, display separate message w/ years to go)
+    -if == display completed countdown clock & congratulatory message  
+-generate and display embed code
+*/
